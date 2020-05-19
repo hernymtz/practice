83fb0197e985f66b9e811e24979e615b3c30c11f
teralgol-max <teralgol@gmail.com>
2020-05-19 12:11:01 -0500

    Basic practice files

---
+++ team_profiles.py
@@ -0,0 +1,9 @@
+# Practice file
+# AxtelLabs Team Profile
+
+
+
+print("Romulo Vidales\n","Age:40\n", "Position: Manager\n",
+    "Description:Java developer. Reader. Fan of fighting video games.\n ")
+
+
